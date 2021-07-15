d2376be38ce2e4528a3ef8768cebb5f020635219
Nishant-l <nishant3151998@gmail.com>
2021-07-15 09:26:52 +0530

    🧭 nav Bar;

--- app/components/populer.js
+++ app/components/populer.js
@@ -1,5 +1,21 @@
-import React from 'react';
+// import * as React from 'react'
+import React from "react";
 
 export default class Popular extends React.Component{
+    render(){
+        const languages = ['ALL','JavaScript','Ruby','Java','CSS','Python'];
+
+        return(
+            <ul className='flex-center'>
+                {languages.map((language)=>(
+                    <li key={language}>
+                        <button className='btn-clear nav-link'>
+                            {language}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        )
+    }
     
 }
--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import Popular from './components/populer';
 import './index.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular /> 
       </div>
     )
   }
