15617b4952ab04a8b6391ac205a70039a92cec8d
Nishant-l <nishant3151998@gmail.com>
2021-07-22 08:23:01 +0530

    ⚙️: Adding state to navbar component;

--- app/components/populer.js
+++ app/components/populer.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 export default class Popular extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            selectedLanguage:'ALL'
+        }
+    }
+
+    currentButton=(language)=>{
+        this.setState({
+            selectedLanguage:language
+        })
+    }
+
     render(){
         const languages = ['ALL','JavaScript','Ruby','Java','CSS','Python'];
 
@@ -8,7 +23,7 @@ export default class Popular extends React.Component{
             <ul className='flex-center'>
                 {languages.map((language)=>(
                     <li key={language}>
-                        <button className='btn-clear nav-link'>
+                        <button className='btn-clear nav-link' style={{color:this.state.selectedLanguage===language?'red':'black'}} onClick={()=>this.currentButton(language)}>
                             {language}
                         </button>
                     </li>
