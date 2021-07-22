b459880d8ab290e16a5618c5bdf8b79c81d30c0e
Nishant-l <nishant3151998@gmail.com>
2021-07-22 19:31:19 +0530

    💁:Seperation of concern language nav;

--- app/components/populer.js
+++ app/components/populer.js
@@ -1,5 +1,24 @@
 import React from "react";
 
+function LanguagesNav({selectedLanguage,currentButton}){
+    const languages = ['ALL','JavaScript','Ruby','Java','CSS','Python'];
+
+        return(
+            <ul className='flex-center'>
+                {languages.map((language)=>(
+                    <li key={language}>
+                        <button 
+                            className='btn-clear nav-link' 
+                            style={{color:selectedLanguage===language?'red':'black'}} 
+                            onClick={()=>currentButton(language)}>
+                            {language}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        )
+}
+
 export default class Popular extends React.Component{
 
     constructor(props){
@@ -17,18 +36,13 @@ export default class Popular extends React.Component{
     }
 
     render(){
-        const languages = ['ALL','JavaScript','Ruby','Java','CSS','Python'];
-
         return(
-            <ul className='flex-center'>
-                {languages.map((language)=>(
-                    <li key={language}>
-                        <button className='btn-clear nav-link' style={{color:this.state.selectedLanguage===language?'red':'black'}} onClick={()=>this.currentButton(language)}>
-                            {language}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <React.Fragment>
+                <LanguagesNav 
+                    selectedLanguage={this.state.selectedLanguage} 
+                    currentButton={this.currentButton}
+                />
+            </React.Fragment>
         )
     }
     
