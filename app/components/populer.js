38ae0028a302b5bd2713cef7cb955899b0f8075f
Nishant-l <nishant3151998@gmail.com>
2022-02-05 14:58:36 +0530

    📲 :Added api to fetch content from github api;

--- app/components/populer.js
+++ app/components/populer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {fetchContent} from '../utils/api';
 
 function LanguagesNav({selectedLanguage,currentButton}){
     const languages = ['ALL','JavaScript','Ruby','Java','CSS','Python'];
@@ -25,23 +26,53 @@ export default class Popular extends React.Component{
         super(props)
 
         this.state={
-            selectedLanguage:'ALL'
+            selectedLanguage:'ALL',
+            error:null,
+            content:null
         }
+        this.isLoding = this.isLoding.bind(this);
+    }
+
+    isLoding() {
+        return this.state.content === null && this.state.error === null;
+    }
+
+    componentDidMount(){
+        this.currentButton(this.state.selectedLanguage);
     }
 
     currentButton=(language)=>{
         this.setState({
-            selectedLanguage:language
+            selectedLanguage:language,
+            error:null,
+            content:null
         })
+        fetchContent(language)
+                    .then((content) => this.setState({
+                        error:null,
+                        content:content
+                    }))
+                    .catch((err) => {
+                        console.warn(err);
+                        this.setState({
+                            err: `error fatching repo ${err}`
+                        })
+                    })
     }
 
     render(){
+        const { selectedLanguage, content, error} = this.state;
+        // console.log(content);
         return(
             <React.Fragment>
                 <LanguagesNav 
-                    selectedLanguage={this.state.selectedLanguage} 
+                    selectedLanguage={selectedLanguage} 
                     currentButton={this.currentButton}
                 />
+                {this.isLoding() && <p>LOADING...</p>}
+                {error && <p>{error}</p>}
+
+                {content && <pre>{JSON.stringify(content,null,2)}</pre>}
             </React.Fragment>
         )
     }
